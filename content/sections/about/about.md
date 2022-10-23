5352d5de982f9eea9bbffc4eda1438f77ce1d9c3
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-10-23 00:55:42 -0700

    updated metadata

--- content/sections/about/about.md
+++ content/sections/about/about.md
@@ -1,6 +1,6 @@
 ---
-imageSrc: "../../images/headshot.png"
-imageAlt: "About Ivan"
+imageSrc: '../../images/headshot.png'
+imageAlt: 'About Ivan'
 ---
 
-I am a full-stack software engineer with a background in web design and a drive to create innovative user friendly technology. My passion for harnessing tech and design has empowered me to develop my skills to build tools and services that produce exceptional products. My strengths with empathy, creativity, and attention to detail amplifies human-centered approaches while delivering vital business needs.
+I am a frontend software developer with a background in web design and a drive to create innovative user friendly technology. My passion for harnessing tech and design has empowered me to develop my skills to build tools and services that produce exceptional products. My strengths with empathy, creativity, and attention to detail amplifies human-centered approaches while delivering vital business needs.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,26 @@
-import React from "react";
+import React from 'react';
 import {
-  AboutSection,
-  ArticlesSection,
-  ContactSection,
-  HeroSection,
-  InterestsSection,
-  Page,
-  ProjectsSection,
-  Seo,
-} from "gatsby-theme-portfolio-minimal";
+	AboutSection,
+	ArticlesSection,
+	ContactSection,
+	HeroSection,
+	InterestsSection,
+	Page,
+	ProjectsSection,
+	Seo,
+} from 'gatsby-theme-portfolio-minimal';
 
 export default function IndexPage() {
-  return (
-    <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
-      <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <AboutSection sectionId="about" heading="About Ivan" />
-        <InterestsSection sectionId="details" heading="Skills" />
-        <ProjectsSection sectionId="projects" heading="Projects" />
-        <ContactSection sectionId="contact" heading="Say Hello" />
-      </Page>
-    </>
-  );
+	return (
+		<>
+			<Seo title="Ivan Miranda Ortega | Software Developer" />
+			<Page useSplashScreenAnimation>
+				<HeroSection sectionId="hero" />
+				<AboutSection sectionId="about" heading="About Ivan" />
+				<InterestsSection sectionId="details" heading="Skills" />
+				<ProjectsSection sectionId="projects" heading="Projects" />
+				<ContactSection sectionId="contact" heading="Say Hello" />
+			</Page>
+		</>
+	);
 }
